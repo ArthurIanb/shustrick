887ff22f416d4ea8e6474b01afe7397af1cd31f8
ArthurIanb <artur.ianb@gmail.com>
2024-03-25 20:18:02 +0500

    create, detail, index done

--- shustrick/articles/models.py
+++ shustrick/articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(verbose_name="title", max_length=100, null=False, blank=False)
+    content = models.TextField(verbose_name="content", null=True, blank=True)
+    is_published = models.BooleanField(verbose_name="is_published")
+    # creator = models.ForeignKey(to=get_user_model(), on_delete=models.DO_NOTHING)
+
+    def get_absolute_url(self):
+        return reverse("articles:detail", kwargs={"pk": self.pk})
+    
+    def __str__(self):
+        return self.title
+    
--- shustrick/articles/views.py
+++ shustrick/articles/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpRequest, Http404
+from django.urls import reverse_lazy
+from .models import Article
+from .forms import ArticleForm
 
 
 def index(request):
-    return render(request, "articles/index.html", {'title': 'Index'})
+    context = {
+        'title': 'Index',
+        'objects': Article.objects.filter(is_published=True),
+    }
+    return render(request, "articles/index.html", context)
 
 
 def detail(request, pk):
-    return render(request, "articles/detail.html", {"title": "Detail", "object_pk": pk})
+    context = {
+        'title': 'Detail',
+        'object': get_object_or_404(Article, pk=pk),
+    }
+    return render(request, "articles/detail.html", context)
 
 
 
-def create(request):
-    return render(request, "articles/create.html", {"title": 'Create page'})
+def create(request: HttpRequest):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse_lazy('articles:index'))
+        raise Http404("error")
+    context = {
+        'title': 'Create page',
+        'form': ArticleForm(),
+    }
+    return render(request, "articles/create.html", context)
 
