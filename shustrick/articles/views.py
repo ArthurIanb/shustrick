f86b67fce33a312ef7fca6a84e76de0c1e5225d1
ArthurIanb <artur.ianb@gmail.com>
2024-03-25 18:58:25 +0500

    templates

--- shustrick/articles/views.py
+++ shustrick/articles/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Index")
+    return render(request, "articles/index.html", {'title': 'Index'})
 
 
-def detail(requst, pk):
-    return HttpResponse(f"Checking article with pk {pk}")
+def detail(request, pk):
+    return render(request, "articles/detail.html", {"title": "Detail", "object_pk": pk})
 
 
-def create(requst):
-    return HttpResponse("Creating article")
+
+def create(request):
+    return render(request, "articles/create.html", {"title": 'Create page'})
+
